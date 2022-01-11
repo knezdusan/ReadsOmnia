3d276b33833a7afffafd4271bd92e793c2a01147
Dusan Knezevic <knez.dusan@gmail.com>
2022-01-11 13:28:11 +0100

    First new commit

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home({listsData}) {
     page: "frontpage",
     bid: "58",
     isbn: "0735222355",
-    title: <h2>The Lincoln Highway:<br />A Novel</h2>,
+    title: <h2>222The Lincoln Highway:<br />A Novel</h2>,
     text: <span><p>#1 NEW YORK TIMES BESTSELLER</p><p>The bestselling author of A Gentleman in Moscow and Rules of Civility and master of absorbing, sophisticated fiction returns with a stylish and propulsive novel set in 1950s America.</p></span>,
     cover: "lincoln-highway-novel-cover.jpg",
   };
