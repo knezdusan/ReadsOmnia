e498dee2c94c7c526556f6f4f72068d3876abdbf
Dusan Knezevic <knez.dusan@gmail.com>
2022-01-13 14:17:28 +0100

    GA code fix2

--- pages/_app.js
+++ pages/_app.js
@@ -1,29 +1,13 @@
 import Script from 'next/script';
 import Layout from '../components/layout/Layout';
+import GA from '../components/lib/GA';
 import "../styles/globals.scss";
 
 export default function MyApp({ Component, pageProps }) {
   return (
     <Layout>
-
-      <Script
-      strategy='lazyOnload'
-      src={`https://www.googletagmanager.com/gtag/js?id=${process.env.RO_GA}`} />
-
-      <Script strategy='lazyOnload' id="ro-ga">
-        {
-          `
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-
-          gtag('config', ${process.env.RO_GA});
-          `
-        }
-      </Script>
-
+      <GA/>
       <Component {...pageProps} />
-
     </Layout>
   )
 }
