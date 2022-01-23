fc184441e6d45c08008e44b0a28c8499cac0ef31
Dusan Knezevic <knez.dusan@gmail.com>
2022-01-23 14:18:10 +0100

    Rotobs.txt added

--- next-sitemap.js
+++ next-sitemap.js
@@ -2,4 +2,12 @@ const siteUrl = "https://readsomnia.com";
 
 module.exports = {
   siteUrl,
-}
+  generateRobotsTxt: true,
+  exclude: ["/test/*"],
+  robotsTxtOptions: {
+    policies: [
+      {userAgent: "*", disallow: "/test/*"},
+      {userAgent: "*", allow: "/"},
+    ],
+  },
+};
