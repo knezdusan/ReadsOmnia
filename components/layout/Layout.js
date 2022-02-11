bb9547e8de1c2c39dafae757a7d3e4feae51fddd
knezdusan <knez.dusan@gmail.com>
2022-02-11 18:48:19 +0100

    Blog - First post added2

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -27,7 +27,7 @@ export default function Layout({ children }) {
   return (
     <div className={styles.page_container}>
       <Head>
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link rel="preload" as="style" href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400&family=Special+Elite&display=swap" rel="stylesheet" />
         <link rel="stylesheet" href='https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,700;1,300;1,400&family=Special+Elite&display=swap' />
 
