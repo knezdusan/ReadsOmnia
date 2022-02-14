148fe5898f4679663105b8c11dac9805877bff8c
knezdusan <knez.dusan@gmail.com>
2022-02-14 15:12:14 +0100

    Blog placeholder posts removed2

--- components/blog/YouTube.js
+++ components/blog/YouTube.js
@@ -6,7 +6,7 @@ export default function YouTube({id}) {
             <iframe
                 className={styles.video}
                 src={`https://www.youtube.com/embed/${id}?autoplay=1&controls=0&loop=1&mute=1`}
-                allowfullscreen
+                allowFullScreen
             />
         </div>
     );
